e85401c7f6ab060553d06c9e71e4a4803b9f8f9b
Ben <benbakhshi@gmail.com>
2013-04-30 13:52:27 +0300

    First commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,35 @@
+class TodosController < ApplicationController
+  def index
+	@todo_items = Todo.all
+	@new_todo = Todo.new
+	render :index
+  end
+
+  def delete
+  
+
+  end
+
+  def add
+  	todo = Todo.create(:todo_item => params[:todo][:todo_item])
+	if !todo.valid?
+		flash[:error] = todo.errors.full_messages.join("<br>").html_safe
+	end
+	redirect_to :action => 'index'
+  end
+
+  def complete
+    params[:todos_checkbox].each do |check|
+		todo_id = check
+			t = Todo.find_by_id(todo_id)
+			t.update_attribute(:completed, true)
+			if t.completed == true
+				t.update_attribute(:completed, false)
+			else t.update_attribute(:completed, true)
+			end
+
+     end
+    redirect_to :action => 'index'
+ end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoApp::Application.config.secret_token = '26a1d537abcad5b044079828c4fb83893fb2771dd86eb058be50505019ffe0c094139b1639ae5c5f675406f712f1e38ccbd38f2f29e06f046f4815d11e2da3b8'
